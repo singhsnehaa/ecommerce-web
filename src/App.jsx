f6c7af6294f775e19ca691ad8440a3885a2a8f0b
Sneha Singh <sneha.kumari@digital.datamatics.com>
2024-08-28 21:48:32 +0530

    react-router-dom implimented

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,23 @@
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
 import "./App.css";
+import Home from "./pages/home/Home";
+import Order from "./pages/order/Order";
+import Cart from "./pages/cart/Cart";
+import Dashboard from "./pages/admin/dashboard/Dashboard";
+import NoPage from "./pages/nopage/NoPage";
 
 function App() {
-  return <div>App</div>;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/order" element={<Order />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/*" element={<NoPage />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
