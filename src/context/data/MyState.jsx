5c2476794ed114af165d11be3bb253bfb2b73f9a
Sneha Singh <sneha.kumari@digital.datamatics.com>
2024-08-29 11:17:55 +0530

    theme created for light and dark mode

--- src/context/data/MyState.jsx
+++ src/context/data/MyState.jsx
@@ -1,12 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import MyContext from "./MyContext";
 
 const MyState = (props) => {
-  const state = { name: "Sneha", rollno: "27" };
-  const color = "red";
+  const [mode, setMode] = useState("light");
+
+  const toggleMode = () => {
+    if (mode === "light") {
+      setMode("dark");
+      document.body.style.backgroundColor = "rgb(17, 24, 39)";
+    } else {
+      setMode("light");
+      document.body.style.backgroundColor = "white";
+    }
+  };
 
   return (
-    <MyContext.Provider value={{ state, color }}>
+    <MyContext.Provider value={{ mode, toggleMode }}>
       {props.children}
     </MyContext.Provider>
   );
